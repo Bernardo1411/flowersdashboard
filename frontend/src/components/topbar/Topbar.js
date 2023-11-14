76675e939698d99a572dc5803e5ca76e4d378113
bernardo1411 <bernardo1411@hotmail.com>
2023-11-14 16:21:56 -0300

    add logout

--- frontend/src/components/topbar/Topbar.js
+++ frontend/src/components/topbar/Topbar.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 import CleanButton from '../cleanButton/CleanButton';
 
@@ -7,6 +8,10 @@ import './Topbar.css';
 
 function Topbar(props) {
   const { onClick } = props;
+
+  const navigate = useNavigate();
+
+  const [openLogout, setOpenLogout] = useState(false);
 
   return (
     <div className="div-topbar">
@@ -18,7 +23,22 @@ function Topbar(props) {
       </div>
       <div className="div_topbar-rightcontent">
         <img alt="menu" src="/assets/images/profile.png" style={{ marginRight: '10px', borderRadius: '100%' }} />
-        <img alt="menu" src="/assets/images/arrow_down.png" />
+        <CleanButton onClick={() => setOpenLogout((state) => !state)}>
+          <img alt="menu" src="/assets/images/arrow_down.png" />
+        </CleanButton>
+        {
+          openLogout && (
+          <div className="logout_div-topbar">
+            <CleanButton onClick={() => {
+              localStorage.removeItem('token');
+              navigate('/login');
+            }}
+            >
+              Sair
+            </CleanButton>
+          </div>
+          )
+        }
       </div>
     </div>
   );
